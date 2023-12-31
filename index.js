9799aad3daa46684fb98171a6099aaacd676043c
metalfrenzy747 <allenjeremy747@gmail.com>
2023-12-30 20:17:12 -0600

    success

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const main= document.getElementById("main")
+main.remove("main")
+let newHeader= document.createElement("h1")
+newHeader.id= "victory"
+newHeader.textContent= "metalfrenzy747 is the champion"
